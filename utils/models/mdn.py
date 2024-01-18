84b7035dfee6bc6d79f95b928d827af1f3fb40e8
Alexander Krauck <alexander.krauck@gmail.com>
2024-01-18 12:13:39 +0100

    Added Softplus to MDN

--- utils/models/mdn.py
+++ utils/models/mdn.py
@@ -30,9 +30,31 @@ class MDN(ConditionalDensityEstimator):
         dropout_rate: float = 0.2,
         activation_function: str = "relu",
         distribution_type: str = "gaussian",
-        tanh_std_stability: Optional[float] = 3.0,
+        std_stability_mode: str = "tanh",
+        tanh_std_stability: float = 3.0,
         **kwargs,
     ):
+        """
+        
+        Parameters
+        ----------
+        train_data_module : 
+            The train data module used to train the model. This is used to get the mean and std of the data to normalize the input and output.
+        n_hidden : list
+            List of hidden layer sizes.
+        n_distributions : int
+            Number of distributions to use.
+        dropout_rate : float, optional
+            Dropout rate, by default 0.2
+        activation_function : str, optional
+            Activation function to use, by default "relu"
+        distribution_type : str, optional
+            Distribution type to use, by default "gaussian"
+        std_stability_mode : str, optional
+            Mode to use for std stability. Can be "none", "tanh" or "softplus", by default "none"
+        tanh_std_stability : Optional[float]
+            Tanh std stability value, by default 3.0. Only used if std_stability_mode is "tanh"
+        """
         super().__init__()
 
         activation_function = activation_function.lower()
@@ -47,6 +69,15 @@ class MDN(ConditionalDensityEstimator):
         self.mean_y, self.std_y = train_data_module.train_dataset.scaler_y
 
         self.tanh_std_stability = tanh_std_stability
+        self.std_stability_mode = std_stability_mode.lower()
+        if self.std_stability_mode not in ["none", "tanh", "softplus"]:
+            raise ValueError(
+                f"std_stability_mode must be one of 'none', 'tanh' or 'softplus', but is {self.std_stability_mode}"
+            )
+        if self.std_stability_mode == "tanh" and self.tanh_std_stability is None:
+            raise ValueError(
+                "tanh_std_stability must be set if std_stability_mode is tanh"
+            )
 
         self.mean_x = nn.Parameter(self.mean_x, requires_grad=False)
         self.std_x = nn.Parameter(self.std_x, requires_grad=False)
@@ -54,7 +85,13 @@ class MDN(ConditionalDensityEstimator):
         self.std_y = nn.Parameter(self.std_y, requires_grad=False)
 
         n_inputs = train_data_module.train_dataset.x.shape[1]
-        self.distribution_class = DISTRIBUTION_MAP[distribution_type.lower()]
+        distribution_type = distribution_type.lower()
+        if distribution_type in DISTRIBUTION_MAP:
+            self.distribution_class = DISTRIBUTION_MAP[distribution_type.lower()]
+        else:
+            raise ValueError(
+                f"Distribution type {distribution_type} not supported."
+            )
         self.n_distributions = n_distributions
         self.mlp = MLP(
             n_inputs,
@@ -71,10 +108,13 @@ class MDN(ConditionalDensityEstimator):
         mlp_out = self.mlp(x)
         logits, mu, log_sigma = torch.split(mlp_out, self.n_distributions, dim=1) 
 
-        if self.tanh_std_stability:
+        if self.std_stability_mode == "tanh":
             log_sigma = F.tanh(log_sigma) * self.tanh_std_stability #TODO !!!!!!!!!!!!!!! justify that. its intended to be a hack to prevent sigmas from getting too large
-
-        sigma = torch.exp(log_sigma) #this can lead to instability easily as sigmas can get extremely large
+            sigma = torch.exp(log_sigma)
+        elif self.std_stability_mode == "softplus":
+            sigma = F.softplus(log_sigma)
+        else:
+            sigma = torch.exp(log_sigma) #this can lead to instability easily as sigmas can get extremely large
 
         weights = F.softmax(logits, dim=1)
 
